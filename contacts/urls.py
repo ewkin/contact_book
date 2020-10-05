4f39e59e8e43266ca6b5b345a6f6f0dcf9300950
ewkin <kimignat@gmail.com>
2020-10-05 10:33:26 +0600

    added contact page

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.contact_list, name='post_list'),
+    path('contact/<int:pk>/', views.contact_detail, name='contact_detail'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Contact
+from django.shortcuts import render, get_object_or_404
 
 
 def contact_list(request):
     contacts = Contact.objects.filter()
     return render(request, 'contacts/contact_list.html', {'contacts': contacts})
+
+
+def contact_detail(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    return render(request, 'contacts/contact_detail.html', {'contact': contact})
