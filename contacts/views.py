70718ae346150bd41870616f4cebb041040f21d6
ewkin <kimignat@gmail.com>
2020-10-05 09:57:30 +0600

    Modified templates to display posts from database.

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Contact
 
 
 def contact_list(request):
-    return render(request, 'contacts/contact_list.html', {})
+    contacts = Contact.objects.filter()
+    return render(request, 'contacts/contact_list.html', {'contacts': contacts})
