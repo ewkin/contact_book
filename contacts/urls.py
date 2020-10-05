6eff85d0fda139e7fc6f788962a70dc4ee87f858
ewkin <kimignat@gmail.com>
2020-10-05 12:33:06 +0600

    added chnage form

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.contact_list, name='post_list'),
     path('contact/<int:pk>/', views.contact_detail, name='contact_detail'),
+    path('contact/new/', views.contact_new, name='contact_new'),
+    path('contact/<int:pk>/edit/', views.contact_edit, name='contact_edit'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Contact
 from django.shortcuts import render, get_object_or_404
+from .forms import ContactForm
+from django.shortcuts import redirect
 
 
 def contact_list(request):
@@ -12,3 +14,28 @@ def contact_list(request):
 def contact_detail(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
     return render(request, 'contacts/contact_detail.html', {'contact': contact})
+
+
+def contact_new(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.save()
+            return redirect('contact_detail', pk=contact.pk)
+    else:
+        form = ContactForm()
+    return render(request, 'contacts/contact_new.html', {'form': form})
+
+
+def contact_edit(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.save()
+            return redirect('contact_detail', pk=contact.pk)
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'contacts/contact_new.html', {'form': form})
