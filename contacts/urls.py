b4b3ed293ba0b8d9d59afea2ccea922bfaac3fbd
ewkin <kimignat@gmail.com>
2020-10-12 09:30:42 +0600

    Added authentification

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
+from .views import register
+from django.contrib.auth import views as auth_view
 
 
 urlpatterns = [
@@ -7,4 +10,8 @@ urlpatterns = [
     path('contact/<int:pk>/', views.contact_detail, name='contact_detail'),
     path('contact/new/', views.contact_new, name='contact_new'),
     path('contact/<int:pk>/edit/', views.contact_edit, name='contact_edit'),
+    path('login/', auth_view.LoginView.as_view(), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(), name='logedout'),
+    path('register/', register, name="register"),
+    path('admin/', admin.site.urls),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Contact
 from django.shortcuts import render, get_object_or_404
 from .forms import ContactForm
-from django.shortcuts import redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 def contact_list(request):
@@ -39,3 +40,26 @@ def contact_edit(request, pk):
     else:
         form = ContactForm(instance=contact)
     return render(request, 'contacts/contact_new.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+
+        # Assigning the User Creation form with the Post request object to form variable
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # Save the user
+            form.save()
+            # Getting the values from the form
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            # Authenticate user using the authenticate method
+            user = authenticate(username=username, password=raw_password)
+
+            # After Successfully Authenticated then use login function to login the user
+
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
